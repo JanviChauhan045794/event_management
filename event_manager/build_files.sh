618428c5305f6fa46eec0c60ec097598f5cc4577
JanviChauhan045 <janvi.ch4545@gmail.com>
2025-06-10 21:52:51 +0530

    changes of build_files

--- event_manager/build_files.sh
+++ event_manager/build_files.sh
@@ -7,3 +7,7 @@ python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
 echo "Collecting static files..."
 python3 manage.py collectstatic --noinput
+
+#!/bin/bash
+pip install -r requirements.txt
+python manage.py collectstatic --noinput
